d29d850ad86fee62d9209a973c1237fbc182e504
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-09-15 10:06:51 +0200

    Add files via upload

---
+++ EnemyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question 2.4
+namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+{
+    //Class inherits from CharacterClass
+    abstract class EnemyClass : CharacterClass
+    {
+        protected Random rnd;
+
+        public EnemyClass(int x, int y, int enemyDamage, int hp, char symbol) : base(x, y, symbol)
+        {
+            this.damage = enemyDamage;
+            this.hp = hp;
+            this.maxHP = hp;
+        }
+
+        //Enemy string output method that returns
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} at [{this.X + 1},{this.Y + 1}] ({this.Damage})";
+        }
+    }
+}
