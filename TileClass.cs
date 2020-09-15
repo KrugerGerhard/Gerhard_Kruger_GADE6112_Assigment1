f4ee6df000f14a624ab7d8df3f23cc09fdd5ad49
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-09-15 09:26:41 +0200

    Add files via upload

---
+++ TileClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+{
+    //Class that serves as the base for Tiles
+    abstract class TileClass
+    {
+        protected int x;
+        protected int y;
+        protected TileType tileType;
+
+        public TileClass(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public enum TileType
+        {
+            Hero,
+            Enemy,
+            Gold, 
+            Weapon,
+        }
+
+        //Vaulues
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public TileType TheTileType
+        {
+            get { return tileType; }
+            set { tileType = value; }
+
+        }
+
+    }
+}
