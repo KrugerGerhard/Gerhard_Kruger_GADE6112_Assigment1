c1a3853bd4e3ddfcbac7aec290d6e2c3f7e35335
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-09-15 09:27:50 +0200

    Add files via upload

---
+++ EmptyTileClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+{
+    class EmptyTileClass : TileClass
+    {
+        public EmptyTileClass(int x, int y) : base(x, y)
+        {
+
+        }
+    }
+}
+    
