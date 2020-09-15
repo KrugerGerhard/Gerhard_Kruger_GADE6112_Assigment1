80f07dcba8a1613acc47db1d0effe38e953781c4
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-09-15 11:45:39 +0200

    Add files via upload

---
+++ GameEngineForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Question 3.3
+namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+{
+    class GameEngineForm
+    {
+        //Declarations of char
+        private MapClass map;
+        private static readonly char hero = 'H';
+        private static readonly char empty = '.';
+        private static readonly char goblin = 'G';
+        private static readonly char obstacle = 'X';
+
+        //Map object constructor
+        public GameEngineForm(int minWidth, int maxWidth, int minHeight, int maxHeight, int numOfEnemies)
+        {
+            map = new MapClass (minWidth, maxWidth, minHeight, maxHeight, numOfEnemies);
+        }
+
+        //Properties for the construtor above
+        public MapClass Map
+        {
+            get { return Map; }
+            set { map = value; }
+        }
+
+        //Method for player movement
+        public bool MovePlayer(CharacterClass.Movement direction)
+        {
+            if (map.Hero.ReturnMove(direction) == CharacterClass.Movement.None)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
