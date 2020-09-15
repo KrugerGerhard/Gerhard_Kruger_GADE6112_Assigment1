2d95e184e7b54c797930da089c0c8600634bd3f6
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-09-15 09:30:54 +0200

    Add files via upload

---
+++ ObstacleClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+{
+    //Class inherits Tile
+    class ObstacleClass : TileClass
+    {
+        //Constructor
+        public ObstacleClass(int x, int y) : base(x, y)
+        {
+
+        }
+    }
+}
