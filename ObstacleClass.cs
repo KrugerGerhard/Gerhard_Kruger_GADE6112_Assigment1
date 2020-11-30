720d1af50e2b3964e73cbc68cd0d3014a8afb8d6
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-11-30 20:03:07 +0200

    Update ObstacleClass.cs

--- ObstacleClass.cs
+++ ObstacleClass.cs
@@ -1,18 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gerhard_Kruger_19343413_GADE6112_Assigment1_GoblinSlayer
+namespace Rougelike_Game
 {
-    //ObstacleClass inherits it's information from TileClass
-    class ObstacleClass : TileClass
+    class Obstacle : Tile //Class for the obstacle tile
     {
         //Constructor
-        public ObstacleClass(int x, int y) : base(x, y)
+        public Obstacle (int x, int y) : base(x, y, 'X')
         {
 
         }
+
     }
+    
 }
